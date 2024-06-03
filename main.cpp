87aeff9fd97bd889d751780fb62437477d530e4c
Jacob Nickerson <jacobnickerson817@gmail.com>
2024-06-03 19:45:58 -0400

    Refactored most headers to use rtweekend, changed main to use new classes (sphere, hittable, hittable_list)

--- main.cpp
+++ main.cpp
@@ -1,29 +1,13 @@
-#include "color.h"
-#include "ray.h"
-#include "vec3.h"
+#include "rtweekend.h"
+#include "hittable.h"
+#include "hittable_list.h"
+#include "sphere.h"
 
-#include <iostream>
 
-
-double hit_sphere(const point3& center, double radius, const ray& r) {  // returns a double, if > 0 then double is used for surface normals
-    vec3 oc = center - r.origin();
-    auto a = r.direction().length_squared();
-    auto h = dot(r.direction(), oc);
-    auto c = oc.length_squared() - radius*radius;
-    auto discriminant = h*h - a*c;
-
-    if (discriminant < 0) {  // imaginary roots -> no intersection
-        return -1;
-    } else {
-        return (h - sqrt(discriminant)) / a;
-    }
-}
-
-color ray_color(const ray& r) {
-    auto t = hit_sphere(point3(0,0,-1), 0.5, r);
-    if (t > 0) {
-        vec3 normal = unit_vector(r.at(t) - vec3(0,0,-1));
-        return 0.5*color(normal.x()+1, normal.y()+1, normal.z()+1);
+color ray_color(const ray& r, const hittable& world) {
+    hit_record rec;
+    if (world.hit(r, 0, infinity, rec)) {
+        return 0.5 * (rec.normal + color(1,1,1));
     }
 
     vec3 unit_direction = unit_vector(r.direction());
@@ -40,6 +24,11 @@ int main() {
     int image_width = 2160;
     int image_height = std::max(int(image_width / aspect_ratio),1);  // image height must be at least 1
 
+    // World
+    hittable_list world;
+    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
+    
     // Camera
     auto focal_length = 1.0;
     auto viewport_height = 2.0;
@@ -72,7 +61,7 @@ int main() {
             auto ray_direction = pixel_center - camera_center;
             ray r(camera_center, ray_direction);
 
-            color pixel_color = ray_color(r);
+            color pixel_color = ray_color(r, world);
             write_color(std::cout, pixel_color);
         }
     }
