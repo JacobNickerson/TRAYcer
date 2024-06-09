db5694e13bd82c553111f93fbd5d8c8e1ea1e0b2
Jacob Nickerson <jacobnickerson817@gmail.com>
2024-06-09 16:48:13 -0400

    Added anti-aliasing, this is done by randomly sampling 10 pixels around each ray, implemented new camera methods sample_square which returns a random vector in a unit square at Z=0, and get_ray which uses sample_square to get a random vector constructs a camera ray from origin pointing at the random point

--- rtweekend.h
+++ rtweekend.h
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <limits>
 #include <memory>
+#include <cstdlib>
 
 
 // Constants
@@ -14,6 +15,14 @@ const double pi = 3.1415926535897932385;
 // Utility Functions
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
+}
+
+inline double random_double() {  // Returns a random real number in [0,1)
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double randomdouble(double min, double max) {  // Returns a random real in [min, max)
+    return min + (max-min)*random_double();
 }
 
 // Common Headers
