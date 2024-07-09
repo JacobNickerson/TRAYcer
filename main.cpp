d030a20cec7884c2f3281feac719831e96120638
Jacob Nickerson <jacobnickerson817@gmail.com>
2024-07-09 12:22:26 -0400

    added new materials, Lambertian (diffuse reflections) and metal

--- main.cpp
+++ main.cpp
@@ -11,13 +11,13 @@ int main() {
     hittable_list world;
 
     auto material_ground = make_shared<Lambertian>(color(0.8, 0.8, 0.0));
-    auto material_center = make_shared<Lambertian>(color(0.1, 0.2, 0.5));
-    auto material_left   = make_shared<Metal>(color(0.8, 0.8, 0.8));
-    auto material_right  = make_shared<Metal>(color(0.8, 0.6, 0.2));
+    auto material_center = make_shared<Metal>(color(0.99, 0.0, 0.99));
+    auto material_left   = make_shared<Metal>(color(0.4, 0.0, 0.7));
+    auto material_right  = make_shared<Metal>(color(0.5, 0.3, 0.2));
 
     world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
-    world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.2),   0.5, material_center));
-    world.add(make_shared<sphere>(point3(-1.5,    0.0, -1.0),   0.5, material_left));
+    world.add(make_shared<sphere>(point3( 0.0,    0.0, -2.5),   0.5, material_center));
+    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
     world.add(make_shared<sphere>(point3( 1.5,    0.0, -1.0),   0.5, material_right));
 
     // Camera
